29f973cc154b4e4f1d311005080c93f5a7df3099
Evan <efe2@txstate.edu>
2020-06-18 19:44:10 -0500

    Get Top Artists

---
+++ auth.py
@@ -0,0 +1,32 @@
+import spotipy
+import sys
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy.util as util
+
+username = 'silvertone31'
+
+scope = 'user-top-read'
+
+token = util.prompt_for_user_token(username,
+                           scope,
+                           client_id='184650a74ff247dbb521d6236247b105',
+                           client_secret='583e43282a7e4016b7e9f90c5fd61e51',
+                           redirect_uri='http://localhost:8888/callback')
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+    for sp_range in ['short_term', 'medium_term', 'long_term']:
+        print("range:", sp_range)
+
+        results = sp.current_user_top_artists(time_range=sp_range, limit=20)
+
+        for i, item in enumerate(results['items']):
+            print(i, item['name'])
+        print()
+else:
+    print("Can't get token for", username)
+
+    
+
+
+
